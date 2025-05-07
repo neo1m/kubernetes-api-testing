d5a42b4248c69130e211c704060fe38a1f4f60c3
Matvey Mayorov <mayorovmatvey1@gmail.com>
2025-05-07 15:31:40 +0300

    fix: catch & log fetch errors

--- helpers/nodeHelpers.js
+++ helpers/nodeHelpers.js
@@ -3,37 +3,57 @@ const { kubeFetch } = require('#helpers/kubeClient.js')
 const nodeBasePath = '/api/v1/nodes'
 
 async function getNodeList() {
-  const res = await kubeFetch(nodeBasePath, {
-    method: 'GET'
-  })
-  const body = await res.json()
-  return { status: res.status, body }
+  try {
+    const res = await kubeFetch(nodeBasePath, {
+      method: 'GET'
+    })
+    const body = await res.json()
+    return { status: res.status, body }
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
 }
 
 async function getNode(name) {
-  const res = await kubeFetch(`${nodeBasePath}/${name}`, {
-    method: 'GET'
-  })
-  const body = await res.json()
-  return { status: res.status, body }
+  try {
+    const res = await kubeFetch(`${nodeBasePath}/${name}`, {
+      method: 'GET'
+    })
+    const body = await res.json()
+    return { status: res.status, body }
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
 }
 
 async function deleteNode(name) {
-  const res = await kubeFetch(`${nodeBasePath}/${name}`, { 
-    method: 'DELETE',
-  })
-  const body = await res.json()
-  return { status: res.status, body }
+  try {
+    const res = await kubeFetch(`${nodeBasePath}/${name}`, { 
+      method: 'DELETE',
+    })
+    const body = await res.json()
+    return { status: res.status, body }
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
 }
 
 async function createNode(nodeObject) {
-  const res = await kubeFetch(nodeBasePath, {
-    method: 'POST',
-    body: JSON.stringify(nodeObject),
-    headers: { 'Content-Type': 'application/json' },
-  })
-  const body = await res.json()
-  return { status: res.status, body }
+  try {
+    const res = await kubeFetch(nodeBasePath, {
+      method: 'POST',
+      body: JSON.stringify(nodeObject),
+      headers: { 'Content-Type': 'application/json' },
+    })
+    const body = await res.json()
+    return { status: res.status, body }
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
 }
 
 module.exports = {
