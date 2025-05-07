a413e9b9f9ad5c6898ce09699c822211559e2b5e
Matvey Mayorov <mayorovmatvey1@gmail.com>
2025-05-07 18:38:42 +0300

    feat: updated descriptions

--- tests/node/base-operations.test.js
+++ tests/node/base-operations.test.js
@@ -4,39 +4,40 @@ const { nodeManifest } = require('#helpers/nodeTemplates.js')
 const { nodeTests } = require('#fixtures/testData.js')
 
 const { nodeName } = nodeTests.nodeData
-const WAIT_TIMEOUT = 60000
 
-describe('base operations with Node', () => {
-  test('should create node', async () => {
-    const newNodeManifest = JSON.parse(JSON.stringify(nodeManifest))
-    newNodeManifest.metadata.name = nodeName
-    const { status } = await createNode(newNodeManifest)
-    expect(status).toBe(201)
-  })
+describe('[base operations with Node]', () => {
+  describe('[when managing node lifecycle]', () => {
+    test('should create a new node', async () => {
+      const newNodeManifest = JSON.parse(JSON.stringify(nodeManifest))
+      newNodeManifest.metadata.name = nodeName
+      const { status } = await createNode(newNodeManifest)
+      expect(status).toBe(201)
+    })
 
-  test('should verify node internal / external IP', async () => {
-    const { status, body } = await waitForNodeReadiness(nodeName, WAIT_TIMEOUT)
-    const externalIP = body.status.addresses.find(addr => addr.type === 'ExternalIP')
-    const internalIP = body.status.addresses.find(addr => addr.type === 'InternalIP')
-    expect(status).toBe(200)
-    expect(externalIP).toBeDefined()
-    expect(internalIP).toBeDefined()
-  })
+    test('should have valid internal and external IP addresses', async () => {
+      const { status, body } = await waitForNodeReadiness(nodeName)
+      const externalIP = body.status.addresses.find(addr => addr.type === 'ExternalIP')
+      const internalIP = body.status.addresses.find(addr => addr.type === 'InternalIP')
+      expect(status).toBe(200)
+      expect(externalIP).toBeDefined()
+      expect(internalIP).toBeDefined()
+    })
 
-  test('should verify node labels', async () => {
-    const { status, body } = await getNode()
-    expect(status).toBe(200)
-    expect(body.metadata.name).toBe(nodeName)
-    expect(body.metadata.labels['beta.kubernetes.io/instance-type']).toBe('vps')
-    expect(body.metadata.labels['failure-domain.beta.kubernetes.io/region']).toBe('ru1')
-    expect(body.metadata.labels['failure-domain.beta.kubernetes.io/zone']).toBe('ru1-dc1')
-    expect(body.metadata.labels['node.kubernetes.io/instance-type']).toBe('vps')
-    expect(body.metadata.labels['topology.kubernetes.io/region']).toBe('ru1')
-    expect(body.metadata.labels['topology.kubernetes.io/zone']).toBe('ru1-dc1')
-  })
+    test('should contain correct labels', async () => {
+      const { status, body } = await getNode()
+      expect(status).toBe(200)
+      expect(body.metadata.name).toBe(nodeName)
+      expect(body.metadata.labels['beta.kubernetes.io/instance-type']).toBe('vps')
+      expect(body.metadata.labels['failure-domain.beta.kubernetes.io/region']).toBe('ru1')
+      expect(body.metadata.labels['failure-domain.beta.kubernetes.io/zone']).toBe('ru1-dc1')
+      expect(body.metadata.labels['node.kubernetes.io/instance-type']).toBe('vps')
+      expect(body.metadata.labels['topology.kubernetes.io/region']).toBe('ru1')
+      expect(body.metadata.labels['topology.kubernetes.io/zone']).toBe('ru1-dc1')
+    })
 
-  test('should delete node', async () => {
-    const { status } = await deleteNode(nodeName)
-    expect(status).toBe(200)
+    test('should delete the node', async () => {
+      const { status } = await deleteNode(nodeName)
+      expect(status).toBe(200)
+    })
   })
 })
