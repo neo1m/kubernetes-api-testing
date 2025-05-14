d647b111bae66cdb24d612f69aff837be892094b
Matvey Mayorov <mayorovmatvey1@gmail.com>
2025-05-14 17:45:40 +0300

    feat: moved old tests to dir

---
+++ old-tests/csr/server-approved-csr.test.js
@@ -0,0 +1,45 @@
+const { createCSR, deleteCSR } = require('#helpers/csrHelpers.js')
+const { waitForCSRStatus } = require('#helpers/waitForCSRApproval.js')
+const { generateCSR } = require('#helpers/generateCSR.js')
+const { serverCSR } = require('#helpers/csrTemplates.js')
+const { csrTests } = require('#fixtures/testData.js')
+
+const { nodeName, externalIP, internalIP } = csrTests.nodeData
+const { serverCSRName } = csrTests
+
+describe('[CSR approved]', () => {
+  describe('[when CSR data and API request are valid]', () => {
+    test('should create CSR', async () => {
+      const csrData = {
+        organizationName: 'system:nodes',
+        commonName: `system:node:${nodeName}`,
+        altNames: [
+          { type: 'ip', value: internalIP },
+          { type: 'ip', value: externalIP },
+        ]
+      }
+  
+      const { csrBase64 } = generateCSR(csrData)
+  
+      const newCSR = JSON.parse(JSON.stringify(serverCSR))
+      newCSR.metadata.name = serverCSRName
+      newCSR.spec.request = csrBase64
+      newCSR.spec.username = `system:node:${nodeName}`
+  
+      const { status } = await createCSR(newCSR)
+      expect(status).toBe(201)
+    })
+  
+    test('should approve CSR', async () => {
+      const { status, body } = await waitForCSRStatus(serverCSRName, 'Approved')
+      expect(status).toBe(200)
+      expect(body.metadata.name).toBe(serverCSRName)
+      expect(body.status.conditions[0].type).toBe('Approved')
+    })
+  
+    test('should delete CSR', async () => {
+      const { status } = await deleteCSR(serverCSRName)
+      expect(status).toBe(200)
+    })
+  })
+})
