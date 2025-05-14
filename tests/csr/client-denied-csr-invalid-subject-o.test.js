68f894bc57ff08062f18d31f09268a33cd98ecba
Matvey Mayorov <mayorovmatvey1@gmail.com>
2025-05-15 01:43:03 +0300

    feat: added tests/csr/client-denied-csr-invalid-subject-o.test.js

---
+++ tests/csr/client-denied-csr-invalid-subject-o.test.js
@@ -0,0 +1,302 @@
+const fs = require('fs')
+const path = require('path')
+const https = require('https')
+const fetch = require('node-fetch')
+
+// Базовый конфиг
+const { outputDir, kubeAuthFiles, kube } = require('#root/config.js')
+
+// Тестовые данные
+const { csrTests } = require('#fixtures/testData.js')
+
+// Вспомогательные методы
+const { createDirectoryIfNotExists, removeDirectory } = require('#helpers/common.js')
+
+// Методы для работы с openssl
+const {
+  generateKeys,
+  generateCSR,
+  encodeCSRToBase64,
+  signCertificate,
+} = require('#helpers/openssl.js')
+
+// Пути к временным файлам
+const testFiles = {
+  privateKey: path.join(outputDir, 'client.key'),
+  publicKey: path.join(outputDir, 'client.pub'),
+  csr: path.join(outputDir, 'client.csr'),
+  ext: path.join(outputDir, 'v3.ext'),
+  crt: path.join(outputDir, 'client.crt')
+}
+
+// Адрес API
+const baseURL = `https://${kube.host}:${kube.port}`
+const csrPath = '/apis/certificates.k8s.io/v1/certificatesigningrequests'
+
+// Тестовые данные
+const csrName = csrTests.clientCSRName
+const nodeData = csrTests.nodeData
+
+// Для данных spec.groups запрещено создание CSR
+const csrCreationForbiddenGroups = [
+  {
+    name: 'only one required group (bootstrappers)',
+    groups: ['system:bootstrappers'],
+  },
+  {
+    name: 'no required groups',
+    groups: ['system:authenticated', 'system:nodes'],
+  },
+  {
+    name: 'both groups are similar but incorrect',
+    groups: ['system:bootstrapers', 'system:bootstrappers:kubeadm:default-node-tokenx'],
+  },
+  {
+    name: 'one correct group and one similar group',
+    groups: ['system:bootstrappers', 'system:bootstrappers:kubeadm'],
+  },
+  {
+    name: 'duplicate of one group, missing the other',
+    groups: ['system:bootstrappers', 'system:bootstrappers'],
+  },
+  {
+    name: 'empty group list',
+    groups: [],
+  },
+  {
+    name: 'random groups',
+    groups: ['devs', 'qa'],
+  },
+]
+
+// Для данных spec.groups отклоняется созданный CSR
+const csrDeniedGroups = [
+  {
+    name: 'only one required group (default-node-token)',
+    groups: ['system:bootstrappers:kubeadm:default-node-token'],
+  },
+  {
+    name: 'required groups plus one extra',
+    groups: ['system:bootstrappers', 'system:bootstrappers:kubeadm:default-node-token', 'extra:group'],
+  },
+  {
+    name: 'required groups with prefix',
+    groups: ['kubeadm:system:bootstrappers', 'system:bootstrappers:kubeadm:default-node-token'],
+  },
+  {
+    name: 'required groups with suffix',
+    groups: ['system:bootstrappersx', 'system:bootstrappers:kubeadm:default-node-token'],
+  },
+]
+
+beforeAll(() => {
+  createDirectoryIfNotExists(outputDir)
+})
+
+afterAll(() => {
+  removeDirectory(outputDir)
+})
+
+describe('[CSR denied]', () => {
+  describe.each(csrCreationForbiddenGroups)
+    ('[when Subject Organization in CSR equal "$name"]', ({ name, groups }) => {
+      test('should prepare openssl files', async () => {
+        // Преобразуем массив к формату для генерации CSR
+        const formatGroups = groups.map((group) => `O=${group}`)
+
+        // Subject
+        const subject = [
+          `CN=system:bootstrap:${nodeData.nodeName}`,
+          ...formatGroups,
+        ]
+
+        // Генерируем ключи, конфиги, CSR и сертификат для клиента
+        generateKeys(testFiles.privateKey, testFiles.publicKey)
+        generateCSR(testFiles.privateKey, testFiles.csr, subject)
+        signCertificate(testFiles.csr, testFiles.crt, kubeAuthFiles.caCrt, kubeAuthFiles.caKey)
+
+        // Проверки
+        expect(fs.existsSync(testFiles.privateKey)).toBe(true)
+        expect(fs.existsSync(testFiles.publicKey)).toBe(true)
+        expect(fs.existsSync(testFiles.csr)).toBe(true)
+        expect(fs.existsSync(testFiles.crt)).toBe(true)
+      })
+
+      test('should not create CSR', async () => {
+        // Настройка HTTPS агента с mTLS
+        const httpsAgent = new https.Agent({
+          cert: fs.readFileSync(testFiles.crt),
+          key: fs.readFileSync(testFiles.privateKey),
+          ca: fs.readFileSync(kubeAuthFiles.caCrt),
+          rejectUnauthorized: false,
+        })
+
+        // CSR в формате base64
+        const base64CSR = encodeCSRToBase64(testFiles.csr)
+
+        // API тело запроса
+        const certificateSigningRequest = {
+          apiVersion: "certificates.k8s.io/v1",
+          kind: "CertificateSigningRequest",
+          metadata: {
+            name: csrName
+          },
+          spec: {
+            request: base64CSR,
+            signerName: "kubernetes.io/kube-apiserver-client-kubelet",
+            usages: [
+              "digital signature",
+              "client auth"
+            ],
+          }
+        }
+
+        // Запрос
+        const res = await fetch(`${baseURL}${csrPath}`, {
+          method: 'POST',
+          body: JSON.stringify(certificateSigningRequest),
+          headers: { 'Content-Type': 'application/json' },
+          agent: httpsAgent,
+        })
+        const body = await res.json()
+
+        // Проверки
+        expect(res.status).toBe(403)
+        expect(body.status).toBe('Failure')
+        expect(body.reason).toBe('Forbidden')
+      })
+    })
+
+  describe.each(csrDeniedGroups)
+    ('[when Subject Organization in CSR equal "$name"]', ({ name, groups }) => {
+      test('should prepare openssl files', async () => {
+        // Преобразуем массив к формату для генерации CSR
+        const formatGroups = groups.map((group) => `O=${group}`)
+
+        // Subject
+        const subject = [
+          `CN=system:bootstrap:${nodeData.nodeName}`,
+          ...formatGroups,
+        ]
+
+        // Генерируем ключи, конфиги, CSR и сертификат для клиента
+        generateKeys(testFiles.privateKey, testFiles.publicKey)
+        generateCSR(testFiles.privateKey, testFiles.csr, subject)
+        signCertificate(testFiles.csr, testFiles.crt, kubeAuthFiles.caCrt, kubeAuthFiles.caKey)
+
+        // Проверки
+        expect(fs.existsSync(testFiles.privateKey)).toBe(true)
+        expect(fs.existsSync(testFiles.publicKey)).toBe(true)
+        expect(fs.existsSync(testFiles.csr)).toBe(true)
+        expect(fs.existsSync(testFiles.crt)).toBe(true)
+      })
+
+      test('should create CSR', async () => {
+        // Настройка HTTPS агента с mTLS
+        const httpsAgent = new https.Agent({
+          cert: fs.readFileSync(testFiles.crt),
+          key: fs.readFileSync(testFiles.privateKey),
+          ca: fs.readFileSync(kubeAuthFiles.caCrt),
+          rejectUnauthorized: false,
+        })
+
+        // CSR в формате base64
+        const base64CSR = encodeCSRToBase64(testFiles.csr)
+
+        // API тело запроса
+        const certificateSigningRequest = {
+          apiVersion: "certificates.k8s.io/v1",
+          kind: "CertificateSigningRequest",
+          metadata: {
+            name: csrName
+          },
+          spec: {
+            request: base64CSR,
+            signerName: "kubernetes.io/kube-apiserver-client-kubelet",
+            usages: [
+              "digital signature",
+              "client auth"
+            ],
+          }
+        }
+
+        // Запрос
+        const res = await fetch(`${baseURL}${csrPath}`, {
+          method: 'POST',
+          body: JSON.stringify(certificateSigningRequest),
+          headers: { 'Content-Type': 'application/json' },
+          agent: httpsAgent,
+        })
+        const body = await res.json()
+
+        // Проверки
+        expect(res.status).toBe(201)
+        expect(body.metadata.name).toBe(csrName)
+      })
+
+      test('should deny CSR', async () => {
+        // Настройка HTTPS агента с mTLS
+        const httpsAgent = new https.Agent({
+          cert: fs.readFileSync(testFiles.crt),
+          key: fs.readFileSync(testFiles.privateKey),
+          ca: fs.readFileSync(kubeAuthFiles.caCrt),
+          rejectUnauthorized: false,
+        })
+
+        // Максимальное время ожидания
+        const maxRetryTime = 4000
+        const retryInterval = 2000
+        const startTime = Date.now()
+        let expectedStatus = 'Denied'
+        let lastStatus = ''
+        let body
+
+        // Цикл запросов
+        while (Date.now() - startTime < maxRetryTime) {
+          const res = await fetch(`${baseURL}${csrPath}/${csrName}`, {
+            method: 'GET',
+            agent: httpsAgent,
+          })
+
+          body = await res.json()
+          lastStatus = body.status?.conditions?.[0]?.type || ''
+
+          if (res.status === 200 && lastStatus === expectedStatus) {
+            // Успешный случай
+            expect(res.status).toBe(200)
+            expect(body.metadata.name).toBe(csrName)
+            expect(body.status.conditions[0].type).toBe(expectedStatus)
+            return
+          }
+
+          // Ждём перед следующим запросом
+          await new Promise(resolve => setTimeout(resolve, retryInterval))
+        }
+
+        // Если дошли сюда - значит Approved не получен за отведённое время
+        throw new Error(`Timeout waiting for CSR "${csrName}" to have status "${expectedStatus}" within ${maxRetryTime / 1000} seconds`)
+      })
+
+      test('should delete CSR', async () => {
+        // Настройка HTTPS агента с mTLS (для удаления CSR после тестов используем доступы от основного клиента)
+        const httpsAgent = new https.Agent({
+          cert: fs.readFileSync(kubeAuthFiles.clientCert),
+          key: fs.readFileSync(kubeAuthFiles.clientKey),
+          ca: fs.readFileSync(kubeAuthFiles.caCrt),
+          rejectUnauthorized: false,
+        })
+
+        // Запрос на удаление
+        const res = await fetch(`${baseURL}${csrPath}/${csrName}`, {
+          method: 'DELETE',
+          agent: httpsAgent,
+        })
+        const body = await res.json()
+
+        // Проверки
+        expect(res.status).toBe(200)
+        expect(body.status).toBe('Success')
+        expect(body.details.name).toBe(csrName)
+      })
+    })
+})
