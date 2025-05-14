d23fc55f08e0d057c7a9aa9b4a3eb3459faa96dd
Matvey Mayorov <mayorovmatvey1@gmail.com>
2025-05-14 18:00:31 +0300

    feat: updated readme

--- config.js
+++ config.js
@@ -2,13 +2,16 @@ const path = require('path')
 const os = require('os')
 
 module.exports = {
+  // директория для временных файлов во время тестов
   outputDir: path.resolve(__dirname, 'fixtures', 'tmp'),
+  // ключи и сертификаты для запросов через mTLS
   kubeAuthFiles: {
     caCrt: path.join(os.homedir(), '.minikube/ca.crt'),
     caKey: path.join(os.homedir(), '.minikube/ca.key'),
     clientCert: path.join(os.homedir(), '.minikube/profiles/minikube/client.crt'),
     clientKey: path.join(os.homedir(), '.minikube/profiles/minikube/client.key'),
   },
+  // общие данные для kubernetes API
   kube: {
     host: require('child_process').execSync('minikube ip').toString().trim(),
     port: 8443,
