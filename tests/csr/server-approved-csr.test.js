481f9bcd5f7bbd55722e821d54e518d4c0313cb2
Matvey Mayorov <mayorovmatvey1@gmail.com>
2025-05-15 13:47:15 +0300

    refactor: tests/csr/server-approved-csr.test.js

--- tests/csr/server-approved-csr.test.js
+++ tests/csr/server-approved-csr.test.js
@@ -38,6 +38,21 @@ const csrPath = '/apis/certificates.k8s.io/v1/certificatesigningrequests'
 const csrName = csrTests.serverCSRName
 const nodeData = csrTests.nodeData
 
+const validData = [
+  {
+    name: 'correct subject, san and usages',
+    subject: [
+      `CN=system:node:${nodeData.nodeName}`,
+      `O=system:nodes`,
+    ],
+    san: [
+      `IP:${nodeData.externalIP}`,
+      `IP:${nodeData.internalIP}`,
+    ],
+    usages: ["digital signature", "server auth"]
+  },
+]
+
 beforeAll(() => {
   createDirectoryIfNotExists(outputDir)
 })
@@ -47,138 +62,130 @@ afterAll(() => {
 })
 
 describe('[CSR approved]', () => {
-  describe('[when CSR data and API request are valid]', () => {
-    test.only('should prepare openssl files', async () => {
-      // Subject
-      const subject = [
-        `CN=system:node:${nodeData.nodeName}`,
-        `O=system:nodes`,
-      ]
+  describe.each(validData)
+    ('[when CSR data and API request equal "$name"]', ({ name, subject, san, usages }) => {
+      test('should prepare openssl files', async () => {
+        // Subject
+        const subjectList = subject
 
-      // Subject Alternative Names
-      const sanList = [
-        `IP:${nodeData.externalIP}`,
-        `IP:${nodeData.internalIP}`,
-      ]
+        // Subject Alternative Names
+        const sanList = san
 
-      // Генерируем ключи, конфиги, CSR и сертификат для клиента
-      generateKeys(testFiles.privateKey, testFiles.publicKey)
-      createExtFile(testFiles.ext, sanList)
-      generateCSR(testFiles.privateKey, testFiles.csr, subject, sanList)
-      signCertificate(testFiles.csr, testFiles.crt, kubeAuthFiles.caCrt, kubeAuthFiles.caKey, testFiles.ext)
+        // Генерируем ключи, конфиги, CSR и сертификат для клиента
+        generateKeys(testFiles.privateKey, testFiles.publicKey)
+        createExtFile(testFiles.ext, sanList)
+        generateCSR(testFiles.privateKey, testFiles.csr, subjectList, sanList)
+        signCertificate(testFiles.csr, testFiles.crt, kubeAuthFiles.caCrt, kubeAuthFiles.caKey, testFiles.ext)
 
-      // Проверки
-      expect(fs.existsSync(testFiles.privateKey)).toBe(true)
-      expect(fs.existsSync(testFiles.publicKey)).toBe(true)
-      expect(fs.existsSync(testFiles.ext)).toBe(true)
-      expect(fs.existsSync(testFiles.csr)).toBe(true)
-      expect(fs.existsSync(testFiles.crt)).toBe(true)
-    })
-
-    test('should create CSR', async () => {
-      // Настройка HTTPS агента с mTLS
-      const httpsAgent = new https.Agent({
-        cert: fs.readFileSync(testFiles.crt),
-        key: fs.readFileSync(testFiles.privateKey),
-        ca: fs.readFileSync(kubeAuthFiles.caCrt),
-        rejectUnauthorized: false,
+        // Проверки
+        expect(fs.existsSync(testFiles.privateKey)).toBe(true)
+        expect(fs.existsSync(testFiles.publicKey)).toBe(true)
+        expect(fs.existsSync(testFiles.ext)).toBe(true)
+        expect(fs.existsSync(testFiles.csr)).toBe(true)
+        expect(fs.existsSync(testFiles.crt)).toBe(true)
       })
 
-      // CSR в формате base64
-      const base64CSR = encodeCSRToBase64(testFiles.csr)
-
-      // API тело запроса
-      const certificateSigningRequest = {
-        apiVersion: "certificates.k8s.io/v1",
-        kind: "CertificateSigningRequest",
-        metadata: {
-          name: csrName
-        },
-        spec: {
-          request: base64CSR,
-          signerName: "kubernetes.io/kubelet-serving",
-          usages: [
-            "digital signature",
-            "server auth"
-          ],
-        }
-      }
-
-      // Запрос
-      const res = await fetch(`${baseURL}${csrPath}`, {
-        method: 'POST',
-        body: JSON.stringify(certificateSigningRequest),
-        headers: { 'Content-Type': 'application/json' },
-        agent: httpsAgent,
-      })
-      const body = await res.json()
-
-      // Проверки
-      expect(res.status).toBe(201)
-      expect(body.metadata.name).toBe(csrName)
-    })
-
-    test('should approve CSR', async () => {
-      // Настройка HTTPS агента с mTLS
-      const httpsAgent = new https.Agent({
-        cert: fs.readFileSync(testFiles.crt),
-        key: fs.readFileSync(testFiles.privateKey),
-        ca: fs.readFileSync(kubeAuthFiles.caCrt),
-        rejectUnauthorized: false,
-      })
-
-      // Максимальное время ожидания
-      const maxRetryTime = 60000
-      const retryInterval = 5000
-      const startTime = Date.now()
-      const expectedStatus = 'Approved'
-
-      // Цикл запросов
-      while (Date.now() - startTime < maxRetryTime) {
-        const res = await fetch(`${baseURL}${csrPath}/${csrName}`, {
-          method: 'GET',
-          agent: httpsAgent,
+      test('should create CSR', async () => {
+        // Настройка HTTPS агента с mTLS
+        const httpsAgent = new https.Agent({
+          cert: fs.readFileSync(testFiles.crt),
+          key: fs.readFileSync(testFiles.privateKey),
+          ca: fs.readFileSync(kubeAuthFiles.caCrt),
+          rejectUnauthorized: false,
         })
 
-        const body = await res.json()
-        const lastStatus = body.status?.conditions?.[0]?.type || ''
+        // CSR в формате base64
+        const base64CSR = encodeCSRToBase64(testFiles.csr)
 
-        if (res.status === 200 && lastStatus === expectedStatus) {
-          // Успешный случай
-          expect(res.status).toBe(200)
-          expect(body.metadata.name).toBe(csrName)
-          expect(body.status.conditions[0].type).toBe(expectedStatus)
-          return
+        // API тело запроса
+        const certificateSigningRequest = {
+          apiVersion: "certificates.k8s.io/v1",
+          kind: "CertificateSigningRequest",
+          metadata: {
+            name: csrName
+          },
+          spec: {
+            request: base64CSR,
+            signerName: "kubernetes.io/kubelet-serving",
+            usages: usages,
+          }
         }
 
-        // Ждём перед следующим запросом
-        await new Promise(resolve => setTimeout(resolve, retryInterval))
-      }
+        // Запрос на создание CSR
+        const res = await fetch(`${baseURL}${csrPath}`, {
+          method: 'POST',
+          body: JSON.stringify(certificateSigningRequest),
+          headers: { 'Content-Type': 'application/json' },
+          agent: httpsAgent,
+        })
+        const body = await res.json()
 
-      // Если дошли сюда - значит Approved не получен за отведённое время
-      throw new Error(`Timeout waiting for CSR "${csrName}" to have status "${expectedStatus}" within ${maxRetryTime / 1000} seconds`)
-    })
-
-    test('should delete CSR', async () => {
-      // Настройка HTTPS агента с mTLS (для удаления CSR после тестов используем доступы от основного клиента)
-      const httpsAgent = new https.Agent({
-        cert: fs.readFileSync(kubeAuthFiles.clientCert),
-        key: fs.readFileSync(kubeAuthFiles.clientKey),
-        ca: fs.readFileSync(kubeAuthFiles.caCrt),
-        rejectUnauthorized: false,
+        // Проверки
+        expect(res.status).toBe(201)
+        expect(body.metadata.name).toBe(csrName)
       })
 
-      // Запрос на удаление
-      const res = await fetch(`${baseURL}${csrPath}/${csrName}`, {
-        method: 'DELETE',
-        agent: httpsAgent,
-      })
-      const body = await res.json()
+      test('should approve CSR', async () => {
+        // Настройка HTTPS агента с mTLS
+        const httpsAgent = new https.Agent({
+          cert: fs.readFileSync(testFiles.crt),
+          key: fs.readFileSync(testFiles.privateKey),
+          ca: fs.readFileSync(kubeAuthFiles.caCrt),
+          rejectUnauthorized: false,
+        })
 
-      // Проверки
-      expect(res.status).toBe(200)
-      expect(body.status).toBe('Success')
-      expect(body.details.name).toBe(csrName)
+        // Максимальное время ожидания
+        const maxRetryTime = 60000
+        const retryInterval = 5000
+        const startTime = Date.now()
+        const expectedStatus = 'Approved'
+
+        // Цикл запросов
+        while (Date.now() - startTime < maxRetryTime) {
+          const res = await fetch(`${baseURL}${csrPath}/${csrName}`, {
+            method: 'GET',
+            agent: httpsAgent,
+          })
+
+          const body = await res.json()
+          const lastStatus = body.status?.conditions?.[0]?.type || ''
+
+          if (res.status === 200 && lastStatus === expectedStatus) {
+            // Успешный случай
+            expect(res.status).toBe(200)
+            expect(body.metadata.name).toBe(csrName)
+            expect(body.status.conditions[0].type).toBe(expectedStatus)
+            return
+          }
+
+          // Ждём перед следующим запросом
+          await new Promise(resolve => setTimeout(resolve, retryInterval))
+        }
+
+        // Если дошли сюда - значит Approved не получен за отведённое время
+        throw new Error(`Timeout waiting for CSR "${csrName}" to have status "${expectedStatus}" within ${maxRetryTime / 1000} seconds`)
+      })
+
+      test('should delete CSR', async () => {
+        // Настройка HTTPS агента с mTLS (для удаления CSR после тестов используем доступы от основного клиента)
+        const httpsAgent = new https.Agent({
+          cert: fs.readFileSync(kubeAuthFiles.clientCert),
+          key: fs.readFileSync(kubeAuthFiles.clientKey),
+          ca: fs.readFileSync(kubeAuthFiles.caCrt),
+          rejectUnauthorized: false,
+        })
+
+        // Запрос на удаление CSR
+        const res = await fetch(`${baseURL}${csrPath}/${csrName}`, {
+          method: 'DELETE',
+          agent: httpsAgent,
+        })
+        const body = await res.json()
+
+        // Проверки
+        expect(res.status).toBe(200)
+        expect(body.status).toBe('Success')
+        expect(body.details.name).toBe(csrName)
+      })
     })
-  })
 })
